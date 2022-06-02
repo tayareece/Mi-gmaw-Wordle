b0daaf0e706a2dc81461fddd958dc6a4165a0e38
tayareece <106720440+tayareece@users.noreply.github.com>
2022-06-02 14:10:34 -0700

    Update config.ts

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,6 +1,6 @@
 export const CONFIG = {
   tries: 6, // This changes how many tries you get to finish the wordle
-  language: 'YourLanguageHere', // This changes the display name for your language
+  language: "Mi'gmaq", // This changes the display name for your language
   wordLength: 6, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
   author: 'taya reece', // Put your name here so people know who made this Wordle!
   authorWebsite: '@taya.reece', // Put a link to your website or social media here
