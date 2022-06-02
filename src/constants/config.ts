5ec1347f2dd4158d052ff33af2c9323a4050f477
tayareece <106720440+tayareece@users.noreply.github.com>
2022-06-02 14:10:04 -0700

    Update config.ts

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,11 +1,11 @@
 export const CONFIG = {
   tries: 6, // This changes how many tries you get to finish the wordle
   language: 'YourLanguageHere', // This changes the display name for your language
-  wordLength: 5, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
-  author: 'YourNameHere', // Put your name here so people know who made this Wordle!
-  authorWebsite: 'YourLinkHere', // Put a link to your website or social media here
-  wordListSource: 'YourDictionarySource', // Describe the source material for your words here
-  wordListSourceLink: 'YourDictionaryLink', // Put a link to the source material for your words here
+  wordLength: 6, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
+  author: 'taya reece', // Put your name here so people know who made this Wordle!
+  authorWebsite: '@taya.reece', // Put a link to your website or social media here
+  wordListSource: "Mi'kmaq Online Dictionary", // Describe the source material for your words here
+  wordListSourceLink: 'https://www.mikmaqonline.org/', // Put a link to the source material for your words here
   //
   // THESE NEXT SETTINGS ARE FOR ADVANCED USERS
   //
